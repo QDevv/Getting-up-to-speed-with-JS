0146d96fa508a18ca85b4ddfe32921f1e5a160e9
QDevv <abdulkudus333@gmail.com>
2023-07-17 22:51:26 +0100

    we die here

--- test.js
+++ test.js
@@ -2847,3 +2847,15 @@ plus.addEventListener('click', revealAnswers);
 minus.addEventListener('click', revealAnswers);
 
 
+// traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+
